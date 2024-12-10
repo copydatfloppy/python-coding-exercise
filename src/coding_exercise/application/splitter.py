5bd7b6cad1bfa2983c169d3a450daf2cb6f61fea
Ashlin Bouwman <ashlin.bouwman.developer@gmail.com>
2024-12-10 18:18:36 +0800

    Add logic in Splitter for finding equal split length, split segment count and remainder

--- src/coding_exercise/application/splitter.py
+++ src/coding_exercise/application/splitter.py
@@ -21,6 +21,13 @@ class Splitter:
         self.input_cable = cable
         self.__validate()
 
+        # Calculate highest integer equal split lengths and remainder
+        self.__determine_split_segment_length_and_count()
+
+        # Loop for self.equal_segments, create Cable of self.equal_length length
+
+        # If self.remainder, append Cable of self.remainder length
+
         return []
 
     def __validate_times_input(self):
@@ -38,3 +45,21 @@ class Splitter:
             or self.input_cable.length > self.INPUT_CABLE_LENGTH_MAX):
 
             raise ValueError
+
+    def __determine_split_segment_length_and_count(self):
+        # Find highest integer segment length and remainder
+        inital_segments = self.times + 1
+        split_length, remainder_length = divmod(self.input_cable.length, inital_segments)
+
+        # If remainder, check if we can create extra equal segments
+        extra_segments, final_remainder = divmod(remainder_length, split_length)
+
+        # Determine final segment count and equal segment length
+        self.equal_length = split_length
+        self.equal_segments = inital_segments + extra_segments
+
+        if (final_remainder):
+            self.total_segments = self.equal_segments + 1
+            self.remainder = final_remainder
+        else:
+            self.total_segments = self.equal_segments
