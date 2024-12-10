2e96aa22646827ae0622eb58dbccce48c0ba86ad
Ashlin Bouwman <ashlin.bouwman.developer@gmail.com>
2024-12-10 15:29:46 +0800

    Add initial tests for splitter length and remainder logic

--- tests/coding_exercise/application/test_splitter.py
+++ tests/coding_exercise/application/test_splitter.py
@@ -43,3 +43,44 @@ def test_should_raise_error_when_provided_cable_has_length_less_than_two():
 
 def test_should_raise_error_when_provided_cable_has_length_greater_than_maximum():
     assert_that(Splitter().split).raises(ValueError).when_called_with(Cable(2048, "coconuts"), 1)
+
+
+# Test resulting array of cables for count and cable length
+def test_should_split_cable_length_of_ten_to_two_cables_of_five():
+    cables = Splitter().split(Cable(10, "coconuts"), 1)
+
+    assert_that(len(cables)).is_equal_to(2)
+    assert_that(cables[0].length).is_equal_to(5)
+    assert_that(cables[1].length).is_equal_to(5)
+
+
+def test_should_split_cable_length_of_five_two_times_to_five_cables_with_length_of_one():
+    cables = Splitter().split(Cable(5, "coconuts"), 2)
+
+    assert_that(len(cables)).is_equal_to(5)
+    assert_that(cables[0].length).is_equal_to(1)
+    assert_that(cables[-1].length).is_equal_to(1)
+
+
+def test_should_split_cable_legth_of_ten_three_times_to_five_cables_with_length_of_two():
+    cables = Splitter().split(Cable(10, "coconuts"), 3)
+
+    assert_that(len(cables)).is_equal_to(5)
+    assert_that(cables[0].length).is_equal_to(2)
+    assert_that(cables[-1].length).is_equal_to(2)
+
+# Test remainder Cable length
+# TODO Double check required functionality
+def test_should_split_cable_legth_of_ten_two_times_to_four_cables_including_remainder():
+    cables = Splitter().split(Cable(10, "coconuts"), 2)
+
+    assert_that(len(cables)).is_equal_to(4)
+    assert_that(cables[0].length).is_equal_to(3)
+    assert_that(cables[-1].length).is_equal_to(1)
+
+def test_should_split_cable_of_seventy_three_times_to_five_cables_including_remainder():
+    cables = Splitter().split(Cable(70, "coconuts"), 3)
+
+    assert_that(len(cables)).is_equal_to(5)
+    assert_that(cables[0].length).is_equal_to(17)
+    assert_that(cables[-1].length).is_equal_to(2)
